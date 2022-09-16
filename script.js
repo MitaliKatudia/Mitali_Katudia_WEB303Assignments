4148ea727c700c4997e7601b1a55097b9daa6b2f
MitaliKatudia <MK494@myscc.ca>
2022-09-16 17:10:51 -0400

    Added the comments to the javascript  'script.js' file

--- script.js
+++ script.js
@@ -2,17 +2,28 @@
 	WEB 303 Assignment 1 - jQuery
 	Mitali Katudia
 */
-
+ // Document ready function to start 
 $(document).ready(function() {
 
-//	$("input").keyup(function() {
-	$("input").change(function() {
-		var Salary = $("#yearly-salary").val();
+/*	$("input").keyup(function() { 
+		//-> this is the keyup function but as the event fires everytime the key is up, 
+    		it also outcomes incomplete calculation  */
+
+		// Change function with input selection which selects both salary and percent's input value 
+	$("input").change(function() {   
+		
+		// selected individual inputs with ID css selector and stored in a variable 
+		var Salary = $("#yearly-salary").val();  
 		var Percent = $("#percent").val();
 
+		// the .val method gets the input from the HTML form submission 
+
+
+		// calculation required for the result and stored in a local variable
 		var result = (Salary * Percent)/ 100;
-		result = result.toFixed();
+		result = result.toFixed();  // the toFixed() method will round off the result in the nearest decimal by two places
 		
+		// selected the span HTML tag and replaced the outcome
 		$("#amount").text('$'+ result);
 	  });
 });
