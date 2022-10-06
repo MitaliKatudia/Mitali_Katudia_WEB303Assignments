376d1eb4f5addd537295d81aa1c32affd6ffa5a6
MitaliKatudia <MK494@myscc.ca>
2022-10-05 20:35:46 -0400

    Changed the code to try something different

--- Assignment 2/js/main.js
+++ Assignment 2/js/main.js
@@ -1,57 +1,46 @@
 // // WEB303 Assignment 2
-// $('').load('')
 
-// $('#content-wrapper').click(function() 
-// {
-
-// }
-// );
-	
-$(document).ready(function()
-{
-    $("#content-wrapper a").click(function(e) {
-    e.preventDefault();
-    $("#content").fadeOut();
-    $("#content").empty();
-    var pagetoLoad = $(this).attr("href");
-    if(pagetoLoad=='prospect.html')
-    { 
-        $.ajax({		
-        type: "GET",
-        url: "prospect.html",
-        data:{page:pagetoLoad},
-        dataType: "html",
-        success: function(data) {		
-            $("#content").html(data);
-            $("#content").fadeIn(2000);
-            }
-         })
-    }
-    else if(pagetoLoad=="convert.html"){		
-              $.ajax({
-        type: "GET",
-        url: "convert.html",
-        data:{page:pagetoLoad},
-        dataType: "html",
-        success: function(data) {			
-            $("#content").html(data);
-            $("#content").fadeIn(2000);
-        }
-                         })
-
-     }
-     else if(pagetoLoad=="retain.html"){
-                $.ajax({
-        type: "GET",
-        url: "retain.html",
-        data:{page:pagetoLoad},
-        dataType: "html",
-        success: function(data) {		
-            $("#content").html(data);
-            $("#content").fadeIn(2000);
-        }
-                         })
-            }
-        })
+$(document).ready(function(){
     
+    $("#content-wrapper a#prospect").click(function(){ 
+       
+        $("#content").load("prospect.html", function(responseTxt1, prospect1) {
+            if(prospect1 == "success")
+            {
+                $("#content").fadeIn(1000);
+
+            //$("#convert").fadeOut(1000);
+           // $("#retain").fadeOut(1000);
+            }
+        });
+
+    });
+
+
+    $("#content-wrapper a#convert").click(function(){
+    
+        $("#content").load("convert.html", function(responseTxt2, convert1){
+            if(convert1 == "success") {
+            $("#content").fadeIn(1000);
+           // $("#prospect").fadeOut(1000);
+            //$("#retain").fadeOut(1000);
+            }
+        });
+    });
+
+
+    $("#content-wrapper a#retain").click(function(){
+        //$("#content").fadeOut(1000);
+        $("#content").load("retain.html", function(responseTxt3, retain1){
+            if(retain1 == "success") {
+                $("#content").fadeIn(1000);
+            //$("#convert").fadeOut(1000);
+            //$("#prospect").fadeOut(1000);
+        }
+
+        });
+      
+    });
+
 });
+
