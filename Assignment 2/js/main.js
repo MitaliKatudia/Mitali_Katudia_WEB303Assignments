fb4ba02fcf12fd5da4251ef6ef1d32b4bf3cca8c
MitaliKatudia <MK494@myscc.ca>
2022-10-05 20:47:48 -0400

    Changed some animation

--- Assignment 2/js/main.js
+++ Assignment 2/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
         $("#content").load("prospect.html", function(responseTxt1, prospect1) {
             if(prospect1 == "success")
             {
-                $("#content").fadeIn(1000);
+                $("#content").fadeIn(2000);
 
             //$("#convert").fadeOut(1000);
            // $("#retain").fadeOut(1000);
@@ -21,7 +21,7 @@ $(document).ready(function(){
     
         $("#content").load("convert.html", function(responseTxt2, convert1){
             if(convert1 == "success") {
-            $("#content").fadeIn(1000);
+            $("#content").fadeIn(2000);
            // $("#prospect").fadeOut(1000);
             //$("#retain").fadeOut(1000);
             }
@@ -33,7 +33,7 @@ $(document).ready(function(){
         //$("#content").fadeOut(1000);
         $("#content").load("retain.html", function(responseTxt3, retain1){
             if(retain1 == "success") {
-                $("#content").fadeIn(1000);
+                $("#content").fadeIn(2000);
             //$("#convert").fadeOut(1000);
             //$("#prospect").fadeOut(1000);
         }
